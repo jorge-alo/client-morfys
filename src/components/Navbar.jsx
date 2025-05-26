905860319b30856d9882f443bf3b40c1a37d2716
Jorge Altamirano <alojorge13@gmail.com>
2025-05-26 13:29:07 -0300

    Agrego NavBar.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import '../styles/navbar.css';
+import '../styles/Navbar.css';
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 

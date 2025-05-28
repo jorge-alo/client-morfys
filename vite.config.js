dac44e6d8ec6e0ac72ce08291fb08facb1f038cd
Jorge Altamirano <alojorge13@gmail.com>
2025-05-27 22:34:55 -0300

    modificando el vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,13 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: './index.html', // Asegura que el punto de entrada sea index.html
+    },
   },
-  base: './',
+  base: '/', // Usa '/' en lugar de './' para producción en Vercel
   server: {
-    host: true
-  }
+    host: true,
+    historyApiFallback: true, // Habilita el fallback a index.html en desarrollo
+  },
 });

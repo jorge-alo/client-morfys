adbb91cd1111884212d41bee54ad05fc3bf03a23
Jorge Altamirano <alojorge13@gmail.com>
2025-06-03 16:21:29 -0300

    modificando MapView

--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -18,7 +18,10 @@ L.Icon.Default.mergeOptions({
 const userIcon = new L.Icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-  
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
 });
 
 

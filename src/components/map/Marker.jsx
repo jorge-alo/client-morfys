5ca6d7367e834b8b02f64412cf6cd0c392802aec
Jorge Altamirano <alojorge13@gmail.com>
2025-06-03 10:31:34 -0300

    configurando los iconos parar produccion

--- src/components/map/Marker.jsx
+++ src/components/map/Marker.jsx
@@ -1,6 +1,18 @@
 import { Marker, Popup, Tooltip} from 'react-leaflet';
 import {  useNavigate } from 'react-router-dom';
 
+
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Configura los iconos para producción
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
 export const MarkerView = ({ coordenadas }) => {
   const navigate = useNavigate();
   console.log("estas son las coordenadas en markerview:", coordenadas);

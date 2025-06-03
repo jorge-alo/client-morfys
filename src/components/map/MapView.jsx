127388f6069f026c2d7bb31234ae7ec94480c326
Jorge Altamirano <alojorge13@gmail.com>
2025-06-03 16:10:30 -0300

    modificando mapview

--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -16,7 +16,7 @@ L.Icon.Default.mergeOptions({
 
 // Icono personalizado para el usuario
 const userIcon = new L.Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],

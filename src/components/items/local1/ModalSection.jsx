1042d58030326fac9e74d06c59e92cb722dde239
Jorge Altamirano <alojorge13@gmail.com>
2025-06-18 10:25:58 -0300

    modificando modalsection haciendo que al ir hacia atras se salga del modal

--- src/components/items/local1/ModalSection.jsx
+++ src/components/items/local1/ModalSection.jsx
@@ -8,6 +8,30 @@ import { Variantes } from '../../pedidos/Variantes';
 
 export const ModalSection = ({ idVaner, bannerValue, updateComida, setUpdateComida, pedidosGuarnicion, setPedidosGuarnicion, comidas, setComidas, login, modal, closeModal, valueInput, pedidos, setPedidos, setCheck, setPrice, price, setContValue, contValue, variante, setVariante}) => {
 
+
+     useEffect(() => {
+        if (modal) {
+            // Cuando se abre el modal, agregamos un nuevo estado al historial
+            window.history.pushState({ modalOpen: true }, '');
+
+            const handlePopState = (event) => {
+                if (variante) {
+                    setVariante(false); // Si está abierta la variante, la cerramos primero
+                } else {
+                    closeModal(); // Si no hay variante, cerramos el modal
+                }
+            };
+
+            window.addEventListener('popstate', handlePopState);
+
+            return () => {
+                window.removeEventListener('popstate', handlePopState);
+                // Volvemos al estado anterior del historial
+                if (modal) window.history.back();
+            };
+        }
+    }, [modal, variante]);
+    
     if (!modal) return null;
     const addbanerORAddData = bannerValue ? AddBanner : AddData
     const ContentComponent = login ? addbanerORAddData : variante ? Variantes : Pedidos

8b900cd225938b9f5ecb9854b8c2a8a767948efd
Jorge Altamirano <alojorge13@gmail.com>
2025-06-26 12:43:08 -0300

    modificando mipedido

--- src/components/items/MiPedido.jsx
+++ src/components/items/MiPedido.jsx
@@ -271,14 +271,15 @@ export const MiPedido = ({ idVaner, price, check, pedidos, setPedidos, setCheck,
                             </div>
 
                             {formaEntrega === 'envio' && (
+                                <div className='mensajedeubicacion' style={{ marginTop: '10px' }}>
+                                    <p>Por favor, envíe su ubicación actual desde WhatsApp para mayor precisión en la entrega.</p>
+                                </div>
+                            )}
+                            
+                            {formaEntrega === 'envio' && (
                                 <div style={{ marginTop: '10px' }}>
                                     <label>Dirección:</label>
                                     <PlaceAutocomplete onPlaceSelected={setUbicacion} />
-                                </div>
-                            )}
-                             {formaEntrega === 'envio' && (
-                                <div className='mensajedeubicacion' style={{ marginTop: '10px' }}>
-                                     <p>Por favor, envíe su ubicación actual desde WhatsApp para mayor precisión en la entrega.</p>
                                 </div>
                             )}
 

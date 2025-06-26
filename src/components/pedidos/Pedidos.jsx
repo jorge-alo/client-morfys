559e28524f6ea4a8029804670f70a738f257be89
Jorge Altamirano <alojorge13@gmail.com>
2025-06-26 16:06:29 -0300

    modificando cariantes

--- src/components/pedidos/Pedidos.jsx
+++ src/components/pedidos/Pedidos.jsx
@@ -162,7 +162,7 @@ export const Pedidos = ({ onSuccess, valueInput, setPrice, price, setContValue, 
                                 <h4>{contValue}x</h4>
                                 <h3>{valueInput.name}</h3>
                             </div>
-                            {price == 0 ? "" : ( valueInput.tamanio ? <p>${price}</p> : <p>${valueInput.price}</p> )}                           
+                            {price == 0 ? "" : ( valueInput.tamanio ? <p>${price}</p> : (valueInput.price == 0 ? "" : <p>${valueInput.price}</p>) )}                           
                         </div>
 
                         <h5>{valueInput.description}</h5>

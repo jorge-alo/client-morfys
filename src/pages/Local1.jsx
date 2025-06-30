8d0dfb504743611b92ce4b69c12388680cbc3499
Jorge Altamirano <alojorge13@gmail.com>
2025-06-30 14:04:40 -0300

    modificando cardSection y local1

--- src/pages/Local1.jsx
+++ src/pages/Local1.jsx
@@ -54,8 +54,8 @@ export const Local1 = () => {
     forGetPage();
   }, [name])
 
-  const handleClickCard = ({ id, user_id, name, description, price, categoria, image, variantes, tipoControl, tamanio, controlunidad }) => {
-    const newValueInput = { ...valueInput, comida_id: id, user_id, name, description, price, categoria, tipoControl, controlunidad, tamanio, image, variantes: variantes || [] }
+  const handleClickCard = ({ id, user_id, name, description, price, categoria, image, variantes, tipoControl, tamanio, controlUnidad }) => {
+    const newValueInput = { ...valueInput, comida_id: id, user_id, name, description, price, categoria, tipoControl, controlunidad: controlUnidad, tamanio, image, variantes: variantes || [] }
     console.log("valor de contrlunidad en local1", controlunidad);
     setValueInput(newValueInput);
     setModal(true);
